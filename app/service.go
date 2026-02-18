9c0e66101b73411b4c60437ea1640f4d81aa4a84
Shakiba Moshiri <net@shakiba.net>
2026-02-18 15:42:22 +0330

    Add "context" and "user dash" to app/service

--- app/service.go
+++ app/service.go
@@ -8,45 +8,65 @@ import (
     //"strings"
     //"strconv"
     //"regexp"
-    "context"
+    // "context"
     "log"
     "os"
     "syscall"
     "os/signal"
 
     "github.com/shakibamoshiri/proxgo/config"
+    "github.com/shakibamoshiri/proxgo/user"
 )
-
 
 func service(args []string, pc *config.Pools, dev io.Writer) (err error) {
     config.Log.Debug("args", "=", args)
     config.Log.Debug("io.Writer", "dev", dev)
 
+
     flags := flag.NewFlagSet("app_run", flag.ExitOnError)
-    duration := flags.String("tick", "", "custom time to tick <NUMBER>[s|m|h]")
+    var duration string
+    var uDash bool
+    var runCount int
+    flags.StringVar(&duration, "tick", "", "custom time to tick <NUMBER>[s|m|h]")
+    flags.BoolVar(&uDash, "udash", false, "enable/run user dash")
+    flags.IntVar(&runCount, "runc", 0, "run counter <NUMBER> (stop after N count)")
     flags.Parse(args)
 
-    if *duration == "" {
+    if duration == "" {
         flags.PrintDefaults()
         return
     }
     
-    customTick, err := time.ParseDuration(*duration)
+    customTick, err := time.ParseDuration(duration)
     if err != nil {
-        return fmt.Errorf("service / time.ParseDuration(%v) %w", *duration, err)
+        return fmt.Errorf("service / time.ParseDuration(%v) %w", duration, err)
     }
-    fmt.Printf("ticking every %+v\n", customTick)
+    log.Printf("service is polling every %+v\n", customTick)
 
-    ctx, cancel := context.WithCancel(context.Background())
-    defer cancel()
+    if uDash {
+        nextArgs := []string{"user", "dash"}
+        go user.Parse(nextArgs)
+    }
 
-    // Handle OS signals
-    go func() {
-        sig := make(chan os.Signal, 1)
-        signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
-        <-sig
-        cancel()
-    }()
+    if runCount < 0 {
+        log.Printf("run count cannot be negative %d\n", runCount)
+        os.Exit(0)
+    }
+
+    // ctx, cancel := context.WithCancel(context.Background())
+    // defer cancel()
+
+    // Handle OS osSignals
+    // go func() {
+    //     osSig := make(chan os.Signal, 1)
+    //     <-osSig
+    //     signal.Notify(osSig, os.Interrupt, os.Kill, syscall.SIGTERM)
+    //     cancel()
+    // }()
+
+    osSig := make(chan os.Signal, 1)
+    signal.Notify(osSig, os.Interrupt, os.Kill, syscall.SIGTERM)
+    defer signal.Stop(osSig)
 
     ticker := time.NewTicker(customTick)
     defer ticker.Stop()
@@ -54,18 +74,25 @@ func service(args []string, pc *config.Pools, dev io.Writer) (err error) {
     for {
         err = run(args, pc, dev)
         if err != nil {
-            ticker.Stop()
-            cancel()
-            return err
+            return fmt.Errorf("service / for-run() failure %w", err)
+        }
+        if runCount >= 0 {
+            runCount--
+            if runCount == 0 {
+                config.Log.Warn("run count is zero", "=", runCount)
+                log.Printf("run count is zero %d\n", runCount)
+                return
+            }
         }
 
         select {
         case <-ticker.C:
             // continue
-        case <-ctx.Done():
-            log.Println("Shutting down gracefully")
-            return nil
+        case sig := <-osSig:
+            config.Log.Warn("service / for-run() signal received", "=", sig)
+            return fmt.Errorf("service / for-run() signal received  %v", sig)
         }
     }
+    return
 }
 
